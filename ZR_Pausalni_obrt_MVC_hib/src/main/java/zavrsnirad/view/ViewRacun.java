693fe54c8451b9a47d25a476145fdd5049c9bb6c
delaga <44586692+delaga@users.noreply.github.com>
2019-09-17 20:31:56 +0200

    MVC ZR: Hibernate 2 +View (Start) +method: getEntiteti @ classes +views(gotovi3)

--- ZR_Pausalni_obrt_MVC_hib/src/main/java/zavrsnirad/view/ViewRacun.java
+++ ZR_Pausalni_obrt_MVC_hib/src/main/java/zavrsnirad/view/ViewRacun.java
@@ -58,7 +58,7 @@ public class ViewRacun extends javax.swing.JFrame {
         btnStorniraj = new javax.swing.JButton();
         btnObrisi1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Račun ..."));
 
@@ -82,6 +82,7 @@ public class ViewRacun extends javax.swing.JFrame {
         txtfNapomena.setRows(5);
         jScrollPane1.setViewportView(txtfNapomena);
 
+        rbtUslugeDomaci.setSelected(true);
         rbtUslugeDomaci.setText("Usluge:Domaći");
         rbtUslugeDomaci.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -317,6 +318,7 @@ public class ViewRacun extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void rbtUslugeDomaciActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rbtUslugeDomaciActionPerformed
@@ -358,37 +360,7 @@ public class ViewRacun extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ViewRacun.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ViewRacun.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ViewRacun.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ViewRacun.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ViewRacun().setVisible(true);
-            }
-        });
-    }
+   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnIIspisi;
